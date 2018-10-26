67daf2521331a6be3540f8e2eff9927a0ac4dcc3
ashabooga <bcuellowolffe@gmail.com>
2018-10-26 14:11:33 -0400

    fixing bugs

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 //     var x = document.getElementById("numVBucks").value;
 //     document.getElementById("demo").innerHTML = "You have " + x + " V-Bucks!";
 // }
+
+var countryNames = document.getElementById('flagChoiceText');
+
+function flagTextShow(x) {
+    document.getElementById("flagChoiceText").style.display = "contents";
+}
+function flagTextHide(x) {
+    document.getElementById("flagChoiceText").style.display = "none";
+}
